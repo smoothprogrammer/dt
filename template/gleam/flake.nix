f9a9627ddcabd3ee3cb7e3c3c4851c08f314fc55
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2024-11-25 19:21:39 +0700

    template: add gleam project

---
+++ template/gleam/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Gleam Project";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "gleam-project";
+            shellHook = ''
+              git config pull.rebase true
+              ${pkgs.neo-cowsay}/bin/cowsay -f sage "Gleam Project"
+            '';
+            buildInputs = with pkgs; [
+              editorconfig-checker
+              gleam
+            ];
+          };
+        }
+      );
+    };
+}
