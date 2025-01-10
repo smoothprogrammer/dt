a18cbf4cc3c8cc3122a1a3d4980064100e4a3601
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2025-01-10 16:16:43 +0700

    bin: add java binary

---
+++ bin/java/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Java Binary";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "java-binary";
+            buildInputs = with pkgs; [
+              jdk
+              maven
+              jdt-language-server
+            ];
+          };
+        }
+      );
+    };
+}
