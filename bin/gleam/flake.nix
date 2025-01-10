23846dd7518def4e319a4195d503cde6cc7c8ea9
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2025-01-10 16:12:51 +0700

    bin: add gleam binary

---
+++ bin/gleam/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Gleam Binary";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "gleam-binary";
+            buildInputs = with pkgs; [
+              gleam
+              erlang_27
+              rebar3
+            ];
+          };
+        }
+      );
+    };
+}
