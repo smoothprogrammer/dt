44f49be0898d3bfb3367bc9359e3be12511f80af
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2025-01-10 16:04:28 +0700

    bin: add go binary

---
+++ bin/go/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Go Binary";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "go-binary";
+            buildInputs = with pkgs; [
+              go
+              gopls
+              delve
+            ];
+          };
+        }
+      );
+    };
+}
