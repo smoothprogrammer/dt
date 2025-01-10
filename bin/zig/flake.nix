c01718870764aacb637dcff2a538dd17705f9070
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2025-01-10 16:18:09 +0700

    bin: add zig binary

---
+++ bin/zig/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Zig Binary";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "zig-binary";
+            buildInputs = with pkgs; [
+              zig
+              zls
+            ];
+          };
+        }
+      );
+    };
+}
