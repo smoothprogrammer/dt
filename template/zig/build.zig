89c87b097e4e36b05347cd96a7405437b4e66269
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2025-05-28 19:26:07 +0700

    template/zig: refactor zig build

--- template/zig/build.zig
+++ template/zig/build.zig
@@ -4,26 +4,28 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "zigproject",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    b.installArtifact(exe);
-
-    const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the application");
-    run_step.dependOn(&run_exe.step);
+    {
+        const exe = b.addExecutable(.{
+            .name = "zigproject",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/main.zig"),
+                .target = target,
+                .optimize = optimize,
+            }),
+        });
+        run_step.dependOn(&b.addRunArtifact(exe).step);
+    }
 
-    const unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    {
+        const unit_test = b.addTest(.{
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/main.zig"),
+                .target = target,
+                .optimize = optimize,
+            }),
+        });
+        test_step.dependOn(&b.addRunArtifact(unit_test).step);
+    }
 }
