e69522dd022f3a6643d7093ed65022d64e5a7dce
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2025-05-28 19:26:28 +0700

    template/c: refactor zig build

--- template/c/build.zig
+++ template/c/build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const run = b.step("run", "Run the app");
+    const run_step = b.step("run", "Run the app");
     {
         const exe = b.addExecutable(.{
             .name = "cproject",
@@ -15,6 +15,6 @@ pub fn build(b: *std.Build) void {
         });
         exe.addCSourceFile(.{ .file = b.path("main.c") });
         exe.linkLibC();
-        run.dependOn(&b.addRunArtifact(exe).step);
+        run_step.dependOn(&b.addRunArtifact(exe).step);
     }
 }
